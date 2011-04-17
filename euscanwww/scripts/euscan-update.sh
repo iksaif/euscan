1d391e6389392c5871527970a450247b4426f20e
Corentin Chary <corentincj@iksaif.net>
2011-04-17 22:19:57 +0200

    euscanwww: add example euscan-update script
    Signed-off-by: Corentin Chary <corentincj@iksaif.net>

---
+++ euscanwww/scripts/euscan-update.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+## Update local trees
+# emerge --sync
+# layman -S
+
+## Also update eix database, because we use eix internaly
+# eix-update
+
+## Scan portage (packages, versions)
+# python manage.py scan-portage
+
+## Scan metadata (herds, maintainers, homepages, ...)
+# python manage.py scan-metadata
+
+## Scan uptsream packages
+# python manage.py scan-upstream --all
+# python manage.py list-packages | gparallel --jobs 150% euscan | python manage.py scan-upstream --feed
