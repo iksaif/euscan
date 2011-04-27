244e7d64fdc6ddeb31ad54dfcc415c50672bdebb
Corentin Chary <corentincj@iksaif.net>
2011-04-27 10:14:09 +0200

    scritps: handle local trees
    Signed-off-by: Corentin Chary <corentincj@iksaif.net>

--- euscanwww/scripts/euscan-update.sh
+++ euscanwww/scripts/euscan-update.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+## Setup some vars to use local portage tree
+# export PATH=${HOME}/euscan/:${PATH}
+# export PORTAGE_CONFIGROOT=${HOME}/local
+# export ROOT=${HOME}/local
+# export EIX_CACHEFILE=${HOME}/local/var/cache/eix
+
+## Go to euscanwww dir
+# cd ${HOME}/euscan/euscanwww/
+
 ## Update local trees
-# emerge --sync
-# layman -S
+# emerge --sync --root=${ROOT} --config-root=${PORTAGE_CONFIGROOT}
+# ROOT="/" layman -S --config=${ROOT}/etc/layman/layman.cfg
 
 ## Also update eix database, because we use eix internaly
 # eix-update
@@ -15,4 +24,4 @@
 
 ## Scan uptsream packages
 # python manage.py scan-upstream --all
-# python manage.py list-packages | gparallel --jobs 150% euscan | python manage.py scan-upstream --feed
+# eix --only-names -x | gparallel --jobs 400% euscan | python manage.py scan-upstream --feed
