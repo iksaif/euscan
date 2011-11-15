66e20ebc2650e7de4d32a74fa3db791f6d5da8a7
Corentin Chary <corentincj@iksaif.net>
2011-11-15 16:56:20 +0100

    euscanwww: enable admin
    Signed-off-by: Corentin Chary <corentincj@iksaif.net>

--- euscanwww/euscan/admin.py
+++ euscanwww/euscan/admin.py
@@ -1,5 +1,20 @@
-"""from polls.models import Package
+from euscan.models import Herd, Maintainer, Package, Version, VersionLog, EuscanResult, Log, \
+    WorldLog, CategoryLog, HerdLog, MaintainerLog
 from django.contrib import admin
 
-admin.site.register(Package)
-"""
+admin.site.register(Herd)
+admin.site.register(Maintainer)
+
+class PackageAdmin(admin.ModelAdmin):
+    search_fields = ('category', 'name')
+admin.site.register(Package, PackageAdmin)
+
+admin.site.register(Version)
+admin.site.register(VersionLog)
+admin.site.register(EuscanResult)
+
+admin.site.register(Log)
+admin.site.register(WorldLog)
+admin.site.register(CategoryLog)
+admin.site.register(HerdLog)
+admin.site.register(MaintainerLog)
--- euscanwww/urls.py
+++ euscanwww/urls.py
@@ -6,15 +6,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^euscanwww/', include('euscanwww.foo.urls')),
-
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
-
     (r'^', include('euscan.urls')),
 )
 
