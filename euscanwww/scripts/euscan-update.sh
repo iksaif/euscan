b0664564f32707c514e57286469f291fe4dad6e7
Corentin Chary <corentincj@iksaif.net>
2012-02-14 14:55:11 +0100

    euscan: update TODO and euscan-update.sh
    Signed-off-by: Corentin Chary <corentincj@iksaif.net>

--- euscanwww/scripts/euscan-update.sh
+++ euscanwww/scripts/euscan-update.sh
@@ -11,16 +11,7 @@
 # ROOT="/" layman -S --config=${ROOT}/etc/layman/layman.cfg
 
 ## Generate ebuild cache to speed up eix
-# cd ${ROOT}/var/lib/layman/ && \
-# for overlay in **/; do
-#     [ ! -f ${overlay}profiles/repo_name ] && continue
-#
-#    echo "egencache ${overlay}"
-#    egencache --jobs=8 --rsync \
-#        --repo=$(cat ${overlay}profiles/repo_name) \
-#	--config-root=${PORTAGE_CONFIGROOT} \
-#	--update --update-use-local-desc
-# done
+# emerge --regen --jobs 4 --root=${ROOT} --config-root=${PORTAGE_CONFIGROOT}
 
 ## Also update eix database, because we use eix internaly
 # eix-update
@@ -30,14 +21,15 @@
 
 ## Scan portage (packages, versions)
 # python manage.py scan-portage --all --purge-versions --purge-packages
-# eix --only-names -x | sort --random-sort | gparallel --eta --load 8 --jobs 400% --max-args=64 python manage.py scan-metadata
+# eix --only-names -x | sort --random-sort | parallel --eta --load 8 --jobs 400% --max-args=64 python manage.py scan-metadata
 
 ## Scan metadata (herds, maintainers, homepages, ...)
 # python manage.py scan-metadata --all
 
 ## Scan uptsream packages
 # python manage.py scan-upstream --all
-# eix --only-names -x | gparallel --jobs 400% euscan | python manage.py scan-upstream --feed --purge-versions
+## or
+# eix --only-names -x | parallel --eta --jobs 400% euscan | python manage.py scan-upstream --feed --purge-versions
 
 ## Update counters
 # python manage.py update-counters
