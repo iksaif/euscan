5bd358968a3d8b0fabaea42e5d03bf37d08aafd9
Corentin Chary <corentincj@iksaif.net>
2012-02-20 08:20:54 +0100

    euscan: re-indent blacklists
    Signed-off-by: Corentin Chary <corentincj@iksaif.net>

--- pym/euscan/__init__.py
+++ pym/euscan/__init__.py
@@ -27,33 +27,33 @@ CONFIG = {
 output = EOutput(CONFIG['quiet'])
 
 BLACKLIST_VERSIONS = [
-	# Compatibility package for running binaries linked against a pre gcc 3.4 libstdc++, won't be updated
-	'>=sys-libs/libstdc++-v3-3.4',
+    # Compatibility package for running binaries linked against a pre gcc 3.4 libstdc++, won't be updated
+    '>=sys-libs/libstdc++-v3-3.4',
 ]
 
 BLACKLIST_PACKAGES = [
-	# These kernels are almost dead
-	'sys-kernel/usermode-sources',
-	'sys-kernel/xbox-sources',
-	'sys-kernel/cell-sources',
+    # These kernels are almost dead
+    'sys-kernel/usermode-sources',
+    'sys-kernel/xbox-sources',
+    'sys-kernel/cell-sources',
 ]
 
 SCANDIR_BLACKLIST_URLS = [
-	'mirror://rubygems/(.*)', # Not browsable
-	'mirror://gentoo/(.*)' # Directory too big
+    'mirror://rubygems/(.*)', # Not browsable
+    'mirror://gentoo/(.*)' # Directory too big
 ]
 
 BRUTEFORCE_BLACKLIST_PACKAGES = [
-	'net-zope/plonepopoll' # infinite loop any http://plone.org/products/plonepopoll/releases/*/plonepopoll-2-6-1.tgz link will work
+    'net-zope/plonepopoll' # infinite loop any http://plone.org/products/plonepopoll/releases/*/plonepopoll-2-6-1.tgz link will work
 ]
 
 BRUTEFORCE_BLACKLIST_URLS = [
-	'http://(.*)dockapps.org/download.php/id/(.*)', # infinite loop
-	'http://hydra.nixos.org/build/(.*)', # infinite loop
-	'http://www.rennings.net/gentoo/distfiles/(.*)', # Doesn't respect 404, infinite loop
-        'http://art.gnome.org/download/(.*)', # Doesn't respect 404, infinite loop
-        'http://barelysufficient.org/~olemarkus/(.*)', # Doesn't respect 404, infinite loop
-        'http://olemarkus.org/~olemarkus/(.*)', # Doesn't respect 404, infinite loop
+    'http://(.*)dockapps.org/download.php/id/(.*)', # infinite loop
+    'http://hydra.nixos.org/build/(.*)', # infinite loop
+    'http://www.rennings.net/gentoo/distfiles/(.*)', # Doesn't respect 404, infinite loop
+    'http://art.gnome.org/download/(.*)', # Doesn't respect 404, infinite loop
+    'http://barelysufficient.org/~olemarkus/(.*)', # Doesn't respect 404, infinite loop
+    'http://olemarkus.org/~olemarkus/(.*)', # Doesn't respect 404, infinite loop
 ]
 
 ROBOTS_TXT_BLACKLIST_DOMAINS = [
