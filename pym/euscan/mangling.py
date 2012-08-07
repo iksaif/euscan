5caca5f41f43c853a168c3eb2f16df4eed16dab2
Corentin Chary <corentin.chary@gmail.com>
2012-08-07 09:32:11 +0200

    euscan: default mangling rule is gentoo
    Signed-off-by: Corentin Chary <corentin.chary@gmail.com>

--- pym/euscan/mangling.py
+++ pym/euscan/mangling.py
@@ -44,6 +44,9 @@ def apply_mangling_rules(kind, rules, string):
     return string
 
 def mangle_version(up_pv, options):
+    # Default rule is gentoo when empty
+    if 'versionmangle' not in options or not options['versionmangle']:
+        options['versionmangle'] = ['gentoo']
     return apply_mangling_rules('versionmangle', options, up_pv)
 
 def mangle_url(url, options):
