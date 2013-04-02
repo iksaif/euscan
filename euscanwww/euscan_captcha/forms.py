0225553eeb45db85c7613c616a35cf2cd6559200
Corentin Chary <corentin.chary@gmail.com>
2013-04-02 10:17:11 +0200

    euscan_captcha: fix captcha

--- euscanwww/euscan_captcha/forms.py
+++ euscanwww/euscan_captcha/forms.py
@@ -1,5 +1,10 @@
 from captcha.fields import ReCaptchaField
 from registration.forms import RegistrationForm
+from django.conf import settings
 
 class RegistrationFormCaptcha(RegistrationForm):
-    captcha = ReCaptchaField(attrs={'theme': 'white'})
+    captcha = ReCaptchaField(
+        public_key=settings.RECAPTCHA_PUBLIC_KEY,
+        private_key=settings.RECAPTCHA_PRIVATE_KEY,
+        use_ssl=True,
+        attrs={'theme': 'white'})
